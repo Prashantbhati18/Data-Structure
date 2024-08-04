b72369a1a51c1c1263b140d3b874c47007c5446d
Prashantbhati18 <bhatiprashant198@gmail.com>
2024-08-04 14:56:59 +0530

    Update dsa2.cpp

--- dsa2.cpp
+++ dsa2.cpp
@@ -1,1 +1,8 @@
 Aim - There is an array a, we have to make another array b from the given array a of same size from 1 to n such that b[i]contains the product of elements of a[i] but not the element present that index.
+
+Algorith - 
+1) Make an array a of size n .
+2) Make another array b of size n and have values zero .
+3) Multiply all the elements of array a , say we get m after multiply.
+4) Now divide the answer m with the elements of a at that index and the final answers are elements of array b.
+
