b2a3656420074819627022a6b926e821302d79b0
Prashantbhati18 <bhatiprashant198@gmail.com>
2024-09-29 21:54:03 +0530

    Create dsa11.cpp

---
+++ dsa11.cpp
@@ -0,0 +1,45 @@
+//STACK USING LINKED LIST //
+
+#include<iostream>
+#include<vector>
+#include<string>
+#include<list>
+using namespace std;
+
+template<class T>
+class Stack{
+    list<T> ll;
+    
+public:
+    void push(T val) {
+        ll.push_front(val);
+    }
+
+    void pop () {
+        ll.pop_front();
+    }
+
+    T top() {
+        return ll.front();
+    }
+
+    bool isEmpty() {
+        return ll.size()==0;
+    }
+};
+int main() {
+    Stack<int> s;
+    
+    s.push(2);
+    s.push(3);
+    s.push(4);
+    s.push(5);
+    
+    while(!s.isEmpty()){
+        cout<<s.top() <<" ";
+        s.pop();
+    }
+    return 0;
+
+    
+}
