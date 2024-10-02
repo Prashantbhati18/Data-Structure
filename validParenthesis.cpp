af18c104d164ffaf508702c1212c72a8a2278508
Prashantbhati18 <bhatiprashant198@gmail.com>
2024-10-02 16:16:30 +0530

    Create validParenthesis.cpp

---
+++ validParenthesis.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+#include<stack>
+#include<string>
+using namespace std;
+
+bool isValid(string str) {
+    stack<char> s;
+
+    for(int i=0 ; i<str.size() ; i++) {
+        char ch = str[i] ;
+        if (ch == '(' || ch == '{' || ch == '[') {
+            s.push(ch) ;
+        }
+        else { //closing
+            if (s.empty()) {
+                return false;
+            }
+
+            //match
+            char top = s.top();
+            if (top == '(' && ch == ')' || top == '{' && ch == '}' || top == '[' && ch == ']') {
+                s.pop();
+            }
+            else { //not matched
+                return false;
+            }
+        }
+    }
+    
+    if (s.empty()) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+int main () {
+    string str1 = "([}])"; //invalid
+    string str2 = "([{}])"; //valid
+
+    cout<<isValid(str1) << endl;
+    cout<<isValid(str2) <<endl;
+    return 0;
+}
